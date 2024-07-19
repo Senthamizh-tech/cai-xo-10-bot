952e7871dcd165de551a6568ea010eda5a065ed2
dante851 <vignesh.s98@cognizant.com>
2024-07-19 17:18:36 +0530

    Update SimpleConversationalBot.js
    increased from 5 to 10 secs

--- SimpleConversationalBot.js
+++ SimpleConversationalBot.js
@@ -68,7 +68,7 @@ module.exports = {
         console.log("Stringified data ===> ", JSON.stringify(data));
        return setTimeout(()=>{
                 sdk.sendUserMessage(data, callback)
-        }, 5000);
+        }, 10000);
          // return (
                 // .then(function () {
                 //     //data.message = "Response 2";
