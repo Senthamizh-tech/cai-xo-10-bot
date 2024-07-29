278152834c840afdedb00f024ccd86878d5b1fb1
Senthamizh-tech <in.ent.common.123@gmail.com>
2024-07-29 14:35:48 +0530

    Create helper.js

---
+++ helper.js
@@ -0,0 +1,15 @@
+var xlsx = require("xlsx");
+
+const getExcelData = () => {
+    const workbook = xlsx.readFile(`C:/Users/2243665/Downloads/sampleBatchtesting.xlsx`);  // Step 2
+    let workbook_sheet = workbook.SheetNames;                // Step 3
+    let workbook_response = xlsx.utils.sheet_to_json(        // Step 4
+      workbook.Sheets[workbook_sheet[0]]
+    );
+    // res.status(200).send({                                   // Step 5
+    //   message: workbook_response,
+    // });
+    return workbook_response;
+}
+
+export default getExcelData;
