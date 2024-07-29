251bee6117389954c88eaf42a6d08a4c1d3f1a70
Senthamizh-tech <in.ent.common.123@gmail.com>
2024-07-29 15:07:30 +0530

    Update helper.js

--- helper.js
+++ helper.js
@@ -2,6 +2,7 @@ var xlsx = require("xlsx");
 
 module.exports = {
     getExcelData: function () {
+        console.log("Inside getExcelData ===> ");
         const workbook = xlsx.readFile(`./sampleBatchtesting.xlsx`);  // Step 2
         let workbook_sheet = workbook.SheetNames;                // Step 3
         let workbook_response = xlsx.utils.sheet_to_json(        // Step 4
@@ -10,6 +11,7 @@ module.exports = {
         // res.status(200).send({                                   // Step 5
         //   message: workbook_response,
         // });
+        console.log("before returning getExcelData ===> ");
         return workbook_response;
     }
 }
